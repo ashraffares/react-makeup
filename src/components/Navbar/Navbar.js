a71fc37964ffc5b372c770e0f29a7a514dc39a97
ashraffares <ashraffares090@gmail.com>
2022-02-01 12:07:55 +0200

    render icons in main page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import NavLinks from '../Nav_link/NavLinks';
 import BrandLink from '../Brand_Link';
+import Heart from '../Icons/Heart';
+import Search from '../Icons/Search';
+import Cart from '../Icons/Cart';
 
 const Navbar = () => (
   <nav className="container mx-auto font-krona flex flex-row justify-between py-3">
@@ -10,6 +13,11 @@ const Navbar = () => (
     <div className="flex items-end">
       <NavLinks />
       <div className="flex px-2" />
+      <Heart color="currentColor" />
+
+      <Search color="currentColor" />
+
+      <Cart color="currentColor" />
     </div>
 
   </nav>
